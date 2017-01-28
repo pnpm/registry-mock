fd1c4e3315423a07b09902caa0ae73c872f722a3
zkochan <zoltan.kochan@gmail.com>
2017-01-29 00:49:01 +0200

    feat: add prepare command

--- bin/pnpm-registry-mock.js
+++ bin/pnpm-registry-mock.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 'use strict'
+const cpr = require('cpr')
+const rimraf = require('rimraf')
+const path = require('path')
 const pnpmRegistryMock = require('..')
 
-pnpmRegistryMock()
+if (process.argv[2] === 'prepare') {
+  const registry = path.join(__dirname, '..', 'registry')
+  const storageCache = path.join(registry, 'storage-cache')
+  const storage = path.join(registry, 'storage')
+  rimraf.sync(storage)
+  cpr(
+    storageCache,
+    storage,
+    err => err && console.error(err)
+  )
+} else {
+  pnpmRegistryMock()
+}
