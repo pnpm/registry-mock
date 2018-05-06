d37d4448ee9e00a2adf6291283fd1d06a0cc0c4d
Zoltan Kochan <z@kochan.io>
2018-05-06 15:06:03 +0300

    fix: install-script-example

--- packages/install-script-example/create.js
+++ packages/install-script-example/create.js
@@ -1,4 +1,4 @@
 'use strict'
 const fs = require('fs')
 
-fs.writeFile(process.argv[2] + '.js', 'module.exports = function () {}\n', 'utf8')
+fs.writeFileSync(process.argv[2] + '.js', 'module.exports = function () {}\n', 'utf8')
