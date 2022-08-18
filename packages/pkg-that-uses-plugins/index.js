e1ec266984c0542fadb80cd66178f455025e97d5
Zoltan Kochan <z@kochan.io>
2022-08-18 03:15:41 +0300

    fix: registry-mock

--- packages/pkg-that-uses-plugins/index.js
+++ packages/pkg-that-uses-plugins/index.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 'use strict'
-console.log('My plugin is', require('plugin-example/package.json').name)
+console.log('My plugin is', require('@pnpm.e2e/plugin-example/package.json').name)
