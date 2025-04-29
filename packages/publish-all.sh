7fae1eb7275991dc14af767de6334fae0703187f
Zoltan Kochan <z@kochan.io>
2025-04-29 11:26:59 +0200

    chore: update settings

--- packages/publish-all.sh
+++ packages/publish-all.sh
@@ -12,7 +12,7 @@ for d in **/package.json; do
   cd $(dirname $d);
   # We need to use "npm publish" here in order to publish "@pnpm.e2e/has-pnpm-use-node-version" with the "pnpm.useNodeVersion" field.
   # pnpm would remove the field but we need it for testing.
-  npm publish || exitstatus=$?;
+  npm publish --@jsr:registry=http://localhost:4873/ || exitstatus=$?;
   cd ..;
   if [ $exitstatus -ne 0 ]; then
     break;
