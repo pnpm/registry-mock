dc9bd4d297a300e7be5fb5adb7bd6156d93e3a60
Zoltan Kochan <z@kochan.io>
2026-01-13 17:17:37 +0100

    fix: use dlx to run verdaccio with given node version

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,8 @@ export function start (opts: execa.Options & { useNodeVersion?: string, listen?:
   ]
   if (opts.useNodeVersion) {
     return execa('pnpm', [
-      `--use-node-version=${opts.useNodeVersion}`,
-      'exec',
-      'node',
+      'dlx',
+      `node@runtime:${opts.useNodeVersion}`,
       ...args,
     ], opts)
   }
