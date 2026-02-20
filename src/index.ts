c25ffe446d211e8d5d3e75b7997aec9b9470be8f
Jason Paulos <jasonpaulos@users.noreply.github.com>
2026-02-20 02:39:21 -0500

    fix: add retry to getIntegrity for verdaccio race condition (#30)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import path from 'path'
 import execa from 'execa'
 import { sync as readYamlFile } from 'read-yaml-file'
@@ -47,10 +46,38 @@ export const addDistTag = _addDistTag(REGISTRY_MOCK_PORT)
 export const addUser = _addUser(REGISTRY_MOCK_PORT)
 
 export const getIntegrity = (pkgName: string, pkgVersion: string): string => {
-  return JSON.parse(fs.readFileSync(
-    path.join(locations.storage(), pkgName, 'package.json'),
-    'utf8'
-  )).versions[pkgVersion].dist.integrity
+  const filePath = path.join(locations.storage(), pkgName, 'package.json')
+  const maxRetries = 4
+  let delay = 200 // milliseconds
+  let content: any
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      content = fsx.readJSONSync(filePath)
+      break
+    } catch (err) {
+      if (attempt === maxRetries ||
+        !err ||
+        typeof err !== 'object' ||
+        (
+          !(err instanceof SyntaxError && err.message.endsWith('Unexpected end of JSON input')) &&
+          (err as NodeJS.ErrnoException).code !== 'ENOENT'
+        )
+      ) {
+        throw err
+      }
+      // If verdaccio downloads a package from the uplink registry because it wasn't present, it
+      // will respond to the HTTP request before it finishes writing the package.json file to the
+      // storage directory. There is also a window where the package.json file may exist but be empty 
+      // (see https://github.com/verdaccio/verdaccio/blob/ae0dbff9a549216bf54a0e1646db6bb743a0c960/packages/plugins/local-storage/src/local-fs.ts#L174-L187).
+      // To handle this gracefully, retry reading the file with an exponential backoff strategy if
+      // we encounter a syntax error or file not found error.
+      Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, delay)
+      delay *= 2
+    }
+  }
+  // content should always be defined here, but check just in case
+  if (!content) throw new Error(`Failed to read package.json for ${pkgName}@${pkgVersion} after ${maxRetries} attempts`)
+  return content.versions[pkgVersion].dist.integrity
 }
 
 export { REGISTRY_MOCK_PORT }
