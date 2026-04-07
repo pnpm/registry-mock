ff64c565f931c445959126e84a99342a55216571
Zoltan Kochan <z@kochan.io>
2026-04-07 20:55:48 +0200

    fix: publish

--- packages/publish-all.sh
+++ packages/publish-all.sh
@@ -18,8 +18,8 @@ for d in **/package.json; do
   # Use npm pack + pnpm publish for packages that pnpm can't publish directly:
   # - catalog: protocol specs (pnpm would try to resolve them)
   # - bundleDependencies (pnpm requires hoisted nodeLinker)
-  if grep -q '"catalog:' package.json; then
-    tarball=$(npm pack --ignore-scripts --pack-destination ..)
+  if grep -q '"catalog:\|"bundleDependencies\|"bundledDependencies' package.json; then
+    tarball=$(npm pack --pack-destination ..)
     pnpm publish --no-git-checks --@jsr:registry=http://localhost:4873/ "../$tarball" || exitstatus=$?;
     rm -f "../$tarball"
   else
